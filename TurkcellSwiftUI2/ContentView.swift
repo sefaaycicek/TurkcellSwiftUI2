5d2301a54f244fe5db927d1f44ee5c961df40c65
Sefa Aycicek <sefa.aycicek@enuygun.com>
2024-09-20 16:10:35 +0300

    Initial Commit

---
+++ TurkcellSwiftUI2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TurkcellSwiftUI2
+//
+//  Created by Sefa Aycicek on 20.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TurkcellSwiftUI2/TurkcellSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  TurkcellSwiftUI2App.swift
+//  TurkcellSwiftUI2
+//
+//  Created by Sefa Aycicek on 20.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TurkcellSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
